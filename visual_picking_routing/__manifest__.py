e6d343adf4fd79e7bf19c67d7528cac0d37c918c
interpapa <interpapadavid2811@gmail.com>
2026-08-01 12:56:20 -0400

    Migrate to Odoo 18.0

--- visual_picking_routing/__manifest__.py
+++ visual_picking_routing/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Visual Picking Routing',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Optimized picking routes and visual product identification for warehouse operators.',
     'description': """
         Visual Picking Routing
