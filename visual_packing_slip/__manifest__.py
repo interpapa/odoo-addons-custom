d4877cb69ec66d01b0472c2abf24fd05b4a5e016
interpapa <interpapadavid2811@gmail.com>
2026-08-04 13:16:23 -0400

    Fix python syntax in manifest

--- visual_packing_slip/__manifest__.py
+++ visual_packing_slip/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Visual Packing Slip: Images on Delivery Reports',
     'version': '15.0.1.0.0',
     'summary': 'Prevent packing mistakes. Add large product images to your Odoo delivery slips.',
-    'description': ""\"
+    'description': """
         Visual Packing Slip
         ===================
         This premium module enhances the native Odoo Delivery Slip (Packing Slip):
@@ -12,7 +12,7 @@
         - Reduces mis-packs and shipping errors by giving operators visual confirmation.
         
         Strictly non-destructive and OCA standard compliant.
-    ""\",
+    """,
     'author': 'David Gil',
     'category': 'Inventory/Inventory',
     'license': 'OPL-1',
